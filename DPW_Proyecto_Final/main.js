4a661211607f75d0d3165b9da34c0a14a8b285d7
Mauriciocoreas25 <mauricio.coreas25@itca.edu.sv>
2025-06-09 11:26:26 -0600

    Add files via upload

---
+++ DPW_Proyecto_Final/main.js
@@ -0,0 +1,5 @@
+function whatsapp() {
+  window.open("https://wa.link/iahme0", "_blank");
+  window.focus(); // Enfoca la ventana del navegador hacia el enlace
+  return false; // Evita el comportamiento por defecto del botón
+}
